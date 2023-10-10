b5450a607949af5df774d6a35fe504701d04030b
Adam Griswold <adam.griswold@nucor.com>
2023-10-10 21:52:42 +0000

    ✨: add back arrow to example layout

--- app/examples/layout.tsx
+++ app/examples/layout.tsx
@@ -1,6 +1,10 @@
-export default function ExampleLayout({children,}: {children: React.ReactNode;}) {
+import { BackArrow } from "@/components/ui/back-arrow";
+
+export default function ExampleLayout({children}: {children: React.ReactNode;}) {
     return (
       <div className="flex flex-col min-h-screen items-center justify-center">
+        <BackArrow className="absolute left-12 top-12 h-12 w-12 rounded-full" />
+
         {children}
       </div>
     );
