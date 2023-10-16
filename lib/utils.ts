63a2e77751f007d64c0276d976dc09051854e7f4
acgriswold <agriswoldcamp@gmail.com>
2023-10-16 08:19:57 -0400

    feat(utils): add tailwind-merge and classnames packages/ tailwind util

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,3 +1,6 @@
+import clsx, { type ClassValue } from "clsx";
+import { twMerge } from "tailwind-merge";
+
 export function sleep(ms: number): Promise<unknown> {
     return new Promise(resolve => setTimeout(resolve, ms) );
 }
@@ -6,4 +9,8 @@ export async function minDelay<T>(promise: Promise<T>, ms: number) : Promise<T> 
     let [p] = await Promise.all([promise, sleep(ms)])
 
     return p;
+}
+
+export function cn(...args: ClassValue[]) {
+    return twMerge(clsx(args));
 }
